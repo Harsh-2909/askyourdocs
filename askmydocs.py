bf402e000fd1730e75150fb26e373bc048c0337b
Harsh-2909 <harsh29092000@gmail.com>
2025-01-27 19:00:11 +0530

    fix: Fixes issues in embedding creation logic. Issues persist in ConversationalChain

--- askmydocs.py
+++ askmydocs.py
@@ -1,8 +1,8 @@
 import streamlit as st
-from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.text_splitter import CharacterTextSplitter
-from langchain_community.llms import OpenAI
+from langchain_openai.llms import OpenAI
 from langchain.chains import ConversationalRetrievalChain
 from dotenv import load_dotenv
 load_dotenv()
@@ -29,6 +29,8 @@ if submit_button and uploaded_file:
     try:
         # Load the document
         file_text = ""
+        print("Processing uploaded file...")
+        print(f"Uploaded file type: {uploaded_file.type}. Uploaded file name: {uploaded_file.name}")
         if uploaded_file.type == "text/plain":
             file_text = uploaded_file.getvalue().decode("utf-8")
         elif uploaded_file.type == "application/pdf":
@@ -38,22 +40,19 @@ if submit_button and uploaded_file:
                 file_text += page.extract_text()
 
         # Split the document into chunks
+        print("Splitting text into chunks...")
         text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
         texts = text_splitter.split_text(file_text)
 
         # Create embeddings for the chunks
-        # embeddings = OpenAIEmbeddings()
-        # text_embeddings = embeddings.embed_documents(texts)
-        # text_embedding_pairs = zip(texts, text_embeddings)
-        # faiss = FAISS.from_embeddings(text_embedding_pairs, embeddings)
-        embeddings = OpenAIEmbeddings()
-        # db = FAISS.from_documents(texts, embeddings)
+        print("Creating embeddings for the chunks...")
+        embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
         db = FAISS.from_texts(texts, embeddings)
 
         retriever = db.as_retriever()
-        st.session_state.qa_chain = ConversationalRetrievalChain(
+        st.session_state.qa_chain = ConversationalRetrievalChain.from_llm(
             retriever=retriever,
-            llm=OpenAI(temperature=0) # model="gpt-4o"
+            llm=OpenAI(temperature=0, model="gpt-4o-mini")
         )
         st.session_state.file_uploaded = True
         st.success("File uploaded successfully")
@@ -68,7 +67,7 @@ if st.session_state.file_uploaded:
 
     if st.button("Send") and user_input:
         try:
-            response = st.session_state.qa_chain.run(
+            response = st.session_state.qa_chain.invoke(
                 input=user_input, chat_history=st.session_state.chat_history
             )
             st.session_state.chat_history.append((user_input, response))
